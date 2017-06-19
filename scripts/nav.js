e6fd2d1cae3e7a287711af8576b70c331e18ecfc
Anthony2511 <anthonybeaumecker@gmail.com>
2017-06-19 23:54:20 +0200

    Added menu responsive

---
+++ scripts/nav.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $('.nav__item').click(function (e) {
+        e.preventDefault();
+        $(this).next().toggleClass('open');
+        $(this).toggleClass('unfolded');
+    });
+
+    $('.nav__button').click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-unfolded');
+        $('body').toggleClass('with-menu');
+    });
+})(jQuery);
